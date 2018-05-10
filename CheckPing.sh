7140f45d6d5d0dec05869ddefd9d97c2780fed75
modfiles <modfiles05@gmail.com>
2018-05-10 12:31:58 +0800

    Update CheckPing.sh

--- CheckPing.sh
+++ CheckPing.sh
@@ -2,7 +2,7 @@
 target=www.yahoo.com
 count=$( ping -c 1 $target | grep icmp* | wc -l )
 if [ $count -eq 0 ]
-then
-sudo /etc/init.d/vpnclient restart
+	then
+		sudo /etc/init.d/vpnclient restart
 fi
 exit 0
